c6efdb57a70f38447ecd0d3a915382d097eb493b
Ibrahim <103577332@student.swin.edu.au>
2021-10-05 12:16:05 +1100

    good stuff

---
+++ Non challenges/Semester 2 Week 11 - Build & Query Practice Challenge/Sql file for Practice build & query challenge.sql
@@ -0,0 +1,45 @@
+-- Ibrahim Oztas
+-- 103577332
+-- Swinburne PAVE
+-- Cert IV in IT
+
+
+/* 
+  Task 1:
+
+  Relational Schema:
+
+  Subject(SubjCode, Description)
+  PK(SubjCode)
+
+  SubjectOffering(Year, Semester, Fee)
+  PK(Year, Semester)
+
+  Teacher(StaffID, Surname, GivenName)
+  PK(StaffID)
+
+  Student(StudentID, Surname, GivenName, Gender)
+  PK(StudentID)
+
+  Enrolment(DateEnrolled, Grade)
+*/
+
+
+-----------------------------------------------------------------
+
+--  Task 2: 
+  
+CREATE DATABASE Build_&_Query_Challenge_Practice;
+  
+
+
+
+-----------------------------------------------------------------
+
+
+
+
+
+
+
+
